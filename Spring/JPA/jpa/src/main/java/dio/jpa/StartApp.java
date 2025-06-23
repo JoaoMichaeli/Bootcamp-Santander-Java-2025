c0cfe2184e3e41583d57368c83200ff19c7e1c85
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-06-23 14:57:03 -0300

    jpa

---
+++ Spring/JPA/jpa/src/main/java/dio/jpa/StartApp.java
@@ -0,0 +1,30 @@
+package dio.jpa;
+
+import dio.jpa.model.User;
+import dio.jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+
+        user.setName("João");
+        user.setUsername("joao");
+        user.setPassword("j12345");
+
+        repository.save(user);
+
+        for (User u: repository.findAll()){
+            System.out.println(u);
+        }
+
+    }
+}
